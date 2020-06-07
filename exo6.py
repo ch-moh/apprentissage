31e37c233955dacd9106f347d7c17635190fbd95
ch-moh <chaabane.mch2005@gmail.com>
2020-06-07 16:47:22 +0200

    add exo6

---
+++ exo6.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# coding=utf-8
+print ('Point A ?')
+xA = input('Entrez xA : ')
+yA = input('Entrez yA : ')
+print ('Point B ?')
+xB = input('Entrez xB : ')
+yB = input('Entrez yB : ')
+d = (int(xB)-int(xA))**2+(int(yB)-int(yA))**2
+print ('Distance AB =', d) 
